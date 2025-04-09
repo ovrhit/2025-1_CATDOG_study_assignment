f319693db3049bf290df661087631c84dfe45a7f
ovrhit <chomj060528@gmail.com>
2025-04-10 00:12:33 +0900

    Assignment 4_final
    + Completed version of assignment 4_3

--- assignment4/4_3_statistics_ans.cs
+++ assignment4/4_3_statistics_ans.cs
@@ -21,6 +21,63 @@ namespace statistics
             int stdCount = data.GetLength(0) - 1;
             // ---------- TODO ----------
             
+            // Avg scores
+
+            Console.WriteLine("Average scores:");
+            for (int i = 2; i < data.GetLength(1); i++) {
+                double subjectTotal = 0;
+                for (int j = 1; j < 1 + stdCount; j++) {
+                    subjectTotal += double.Parse(data[j,i]);
+                }
+                subjectTotal /= stdCount;
+                Console.WriteLine($"{data[0,i].ToString()}: {subjectTotal, 0:N2}");
+            }
+            
+            // Max & min scores
+            
+            Console.WriteLine("\nMax and min scores:");
+            for (int i = 2; i < data.GetLength(1); i++) {
+                int mx = 0; int mn = 100;
+                for (int j = 1; j < 1 + stdCount; j++) {
+                    if (int.Parse(data[j,i]) > mx) mx = int.Parse(data[j,i]);
+                    if (int.Parse(data[j,i]) < mn) mn = int.Parse(data[j,i]);
+                }
+                Console.WriteLine($"{data[0,i].ToString()}: ({mn}, {mx})");
+            }
+            
+            // Rank by total
+
+            Console.WriteLine("\nStudents rank by total scores:");
+            
+            // Initiate Array total in format of [[total score, place]]
+            int[,] total = new int[stdCount,2];
+            for (int i = 1; i < data.GetLength(0); i++) {
+                int scoreTotal = 0;
+                for (int j = 2; j < data.GetLength(1); j++) {
+                    scoreTotal += int.Parse(data[i,j]);
+                }
+                total[i-1,0] = scoreTotal;
+            }
+
+            for (int i = 0; i < stdCount; i++) {
+                int place = 1;
+                for (int j = 0; j < stdCount; j++) {
+                    if (i != j && total[i,0] < total[j,0]) place++;
+                }
+                total[i,1] = place;
+            }
+
+            // Choosing prefixes and print
+            for (int i = 1; i <= stdCount; i++) {
+                string name = data[i,1];
+                string prefix = "th";
+                int place = total[i-1,1];
+
+                if (place % 10 == 1 && place / 10 != 1) prefix = "st";
+                if (place % 10 == 2 && place / 10 != 1) prefix = "nd";
+
+                Console.WriteLine($"{name}: {place.ToString()}{prefix}");
+            }
             // --------------------
         }
     }
