cef853ca429c3d9885cb774f93081970ecca11d2
ovrhit <chomj060528@gmail.com>
2025-04-10 00:30:14 +0900

    Revert Assignment 4_1

--- assignment4/4_1_calculator_ans.cs
+++ assignment4/4_1_calculator_ans.cs
@@ -50,35 +50,7 @@ namespace calculator
     public class Calculator
     {
         // ---------- TODO ----------
-        public double Calculate(double num1, string op, double num2) {
-            
-            switch (op) {
-                
-                case "+": return num1 + num2;
-                case "-": return num1 - num2;
-                case "*": return num1 * num2;
-                case "**": 
-                    double result = 1;
-                    bool neg = (num2 < 0) ? true : false;
-                    if (neg) {
-                        for (int i = 0; i > num2; i--) {
-                            result /= num1;
-                        }
-                    } else {
-                         for (int i = 0; i < num2; i++) {
-                             result *= num1;
-                         }
-                    }
-                    return result;
-                case "%": return num1 % num2;
-                case "/":
-                    if (num2 == 0) {
-                        throw new DivideByZeroException("Division by zero is not allowed");
-                    } else return num1 / num2;
-                default: throw new InvalidOperationException("Invalid operator");
-            }
         
-        }
         // --------------------
     }
 }
